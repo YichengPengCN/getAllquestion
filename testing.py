b9af246df9fff81be38d4b747892aee4e83940c1
YichengPengCN <13422935195@163.com>
2022-05-02 14:08:57 +0800

    y

--- testing.py
+++ testing.py
@@ -4,9 +4,5 @@ import urllib.parse
 
 # set up local db, just for testing, need to refine for backend
 # "mongodb+srv://3170Project2:fit3170@cluster0.8wuog.mongodb.net/FIT3170?retryWrites=true&w=majority"
-client = pymongo.MongoClient()
-db = client["FIT3170"]
-collection = db["TagInfo"]
-
-for document in collection.find():
-    print (document)
+client = pymongo.MongoClient('mongodb://52.62.92.51:27017')
+print(client.list_database_names())
